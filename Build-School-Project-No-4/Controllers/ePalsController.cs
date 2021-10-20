f7d03d2e0b6bb79f52a03d956bbb4d027aaab388
唐樂維 <le.kentang@gmail.com>
2021-10-20 21:42:01 +0800

    Devlop marge product

--- Build-School-Project-No-4/Controllers/ePalsController.cs
+++ Build-School-Project-No-4/Controllers/ePalsController.cs
@@ -34,16 +34,10 @@ namespace Build_School_Project_No_4.Controllers
             {
                 return RedirectToAction("ePal", "ePals", new { id = 1 });
             }
-            //var ProductCards = _productService.GetProductCardsData(id.Value);
             var GamesDeatils = _productService.GetGamesAllAndDeatils(id.Value);
 
-            GroupViewModel result = new GroupViewModel
-            {
-                GamesDetails = GamesDeatils,
-                //ProductCards = ProductCards
-            };
-            //ViewBag.ProductCard = _productService.GetProductCardsJson(id.Value);
-            return View("ePal",result);
+           
+            return View("ePal", GamesDeatils);
         }
 
         public ActionResult GamesJson(int id)
--- Build-School-Project-No-4/ViewModels/GroupViewModel.cs
+++ Build-School-Project-No-4/ViewModels/GroupViewModel.cs
@@ -12,8 +12,8 @@ namespace Build_School_Project_No_4.ViewModels
 
         public IEnumerable<MemberViewModel> MeetLikes { get; set; }
         //public IEnumerable<ProductViewModel> EPalIndex { get; set; }
-        public ProductViewModel ProductCards { get; set; }
-        public CategoryViewModel GamesDetails{ get; set; }
+        //public ProductViewModel ProductCards { get; set; }
+        //public CategoryViewModel GamesDetails{ get; set; }
         public IEnumerable<ProductViewModel> EPalIndex { get; set; }
 
         public IEnumerable<WalletViewModel> wallets { get; set; }
